651193fbb4adb407c6f1282a1c4e68fe89a47514
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-08 09:27:24 -0600

    feat: Se ha cargado la logica JavaScript de prodcutos

---
+++ main/src/main/resources/static/productos.js
@@ -0,0 +1,6 @@
+function limpiarCampos(){
+    document.getElementById("idProducto").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("precio").value = "";
+    document.getElementById("stock").value = "";
+}
