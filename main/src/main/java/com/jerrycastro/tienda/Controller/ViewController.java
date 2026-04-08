904ed377648fa23d1ff94ab8c2c1244a1fcbb894
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-08 09:26:53 -0600

    feat: Se ha cargado el controlador de las vistas

---
+++ main/src/main/java/com/jerrycastro/tienda/Controller/ViewController.java
@@ -0,0 +1,37 @@
+package com.jerrycastro.tienda.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/productos")
+    public String mostrarProductos(){
+        return "productos";
+    }
+
+    @GetMapping("/sistema")
+    public String mostrarSistema(){
+        return "sistema";
+    }
+
+    @GetMapping("/clientes")
+    public String mostrarClientes(){
+        return "clientes";
+    }
+
+    @GetMapping("/detalleventas")
+    public String mostrarDetalle(){
+        return "detalleventa";
+    }
+
+    @GetMapping("/usuarios")
+    public String mostrarUsuario(){
+        return "usuarios";
+    }
+
+    @GetMapping("/ventas")
+    public String mostrarVentas(){
+        return "ventas";
+    }
+}
