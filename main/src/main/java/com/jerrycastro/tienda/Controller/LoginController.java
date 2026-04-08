2774219cc5a1ed8f846585628b21bafbe3053232
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-08 11:41:45 -0600

    feat: se ha puesto un mensaje de credenciales

--- main/src/main/java/com/jerrycastro/tienda/Controller/LoginController.java
+++ main/src/main/java/com/jerrycastro/tienda/Controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.jerrycastro.tienda.Controller;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,14 +21,18 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password
+                        @RequestParam("password") String password,
+                        HttpSession session,
+                        Model model
                         ){
         String userCheck = "admin";
         String passCheck = "123";
 
         if(userCheck.equals(username) && passCheck.equals(password)){
+            session.setAttribute("usuarioLogueado", username);
             return "redirect:/principal";
         }else{
+            model.addAttribute("loginError", "Credenciales incorrectas");
             return "login";
         }
     }
