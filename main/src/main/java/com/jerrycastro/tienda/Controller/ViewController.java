3cdfcca4a5894425b0b1fc789280eb5b00e2fe73
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-14 11:47:48 -0600

    fix: se ha agregado los controladores y su logica de las vistas html

--- main/src/main/java/com/jerrycastro/tienda/Controller/ViewController.java
+++ main/src/main/java/com/jerrycastro/tienda/Controller/ViewController.java
@@ -1,12 +1,30 @@
 package com.jerrycastro.tienda.Controller;
 
+import com.jerrycastro.tienda.Entity.Productos;
+import com.jerrycastro.tienda.Entity.Ventas;
+import com.jerrycastro.tienda.Service.ProductosService;
+import com.jerrycastro.tienda.Service.VentasService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ViewController {
+
+    private final ProductosService productosService;
+    private final VentasService ventasService;
+
+    public ViewController(ProductosService productosService, VentasService ventasService) {
+        this.productosService = productosService;
+        this.ventasService = ventasService;
+    }
+
     @GetMapping("/productos")
-    public String mostrarProductos(){
+    public String mostrarProductos(Model model){
+        List<Productos> lista = productosService.getAllProductos();
+        model.addAttribute("productos", lista);
         return "productos";
     }
 
@@ -25,13 +43,12 @@ public class ViewController {
         return "detalleventa";
     }
 
-    @GetMapping("/usuarios")
-    public String mostrarUsuario(){
-        return "usuarios";
-    }
+
 
     @GetMapping("/ventas")
-    public String mostrarVentas(){
+    public String mostrarVentas(Model model){
+        List<Ventas> lista = ventasService.getAllVentas();
+        model.addAttribute("ventas", lista);
         return "ventas";
     }
 
