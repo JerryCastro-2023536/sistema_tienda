bdf56de388dcca2aa4dd2ff3d83ddbde46b824e1
JerryCastro-2023536 <jcastro-2023536@kinal.edu.gt>
2026-04-14 11:45:16 -0600

    feat: Se ha agregado la conexion de los endpoints y la db

--- main/src/main/java/com/jerrycastro/tienda/Controller/LoginController.java
+++ main/src/main/java/com/jerrycastro/tienda/Controller/LoginController.java
@@ -1,14 +1,25 @@
 package com.jerrycastro.tienda.Controller;
 
-import jakarta.servlet.http.HttpSession;
+import com.jerrycastro.tienda.Entity.Usuarios;
+import com.jerrycastro.tienda.Service.UsuariosService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class LoginController {
+
+    private final UsuariosService usuariosService;
+
+    public LoginController(UsuariosService usuariosService) {
+        this.usuariosService = usuariosService;
+    }
+
     @GetMapping("/")
     public String inicio(){
         return "redirect:/acceder";
@@ -19,21 +30,55 @@ public class LoginController {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String login(@RequestParam("username") String username,
-                        @RequestParam("password") String password,
-                        HttpSession session,
-                        Model model
-                        ){
-        String userCheck = "admin";
-        String passCheck = "123";
 
-        if(userCheck.equals(username) && passCheck.equals(password)){
-            session.setAttribute("usuarioLogueado", username);
-            return "redirect:/principal";
-        }else{
-            model.addAttribute("loginError", "Credenciales incorrectas");
+    @PostMapping("/login")
+    public String validar(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          Model model) {
+
+        Usuarios u = usuariosService.login(username, password);
+
+        if (u != null) {
+            return "redirect:/sistema";
+        } else {
+            model.addAttribute("error", "Credenciales incorrectas");
             return "login";
         }
     }
+
+    // REGISTRO
+    @GetMapping("/register")
+    public String registro() {
+        return "register";
+    }
+
+    @PostMapping("/registro")
+    public String guardar(@RequestParam("username") String username,
+                          @RequestParam("password") String password,
+                          Model model) {
+
+        Usuarios u = usuariosService.registrar(username, password);
+
+        if (u == null) {
+            model.addAttribute("error", "Usuario ya existe");
+            return "register";
+        }
+
+        return "redirect:/acceder";
+    }
+
+    // LISTA
+    @GetMapping("/usuarios")
+    public String listar(Model model) {
+        List<Usuarios> lista = usuariosService.getAllUsuarios();
+        model.addAttribute("usuarios", lista);
+        return "usuarios";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable int id) {
+        usuariosService.deleteUsuarios(id);
+        return "redirect:/lista";
+    }
+
 }
