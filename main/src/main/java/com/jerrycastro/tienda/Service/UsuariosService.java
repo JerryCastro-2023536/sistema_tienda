dd4bba7baab2c2b8a8d8e771b24d2af1ca19a963
JerryCastro <jcastro-2023536@kinal.edu.gt>
2026-04-14 22:17:33 -0600

    feat: Se ha agregado el atributo foto en save

--- main/src/main/java/com/jerrycastro/tienda/Service/UsuariosService.java
+++ main/src/main/java/com/jerrycastro/tienda/Service/UsuariosService.java
@@ -2,7 +2,9 @@ package com.jerrycastro.tienda.Service;
 
 import com.jerrycastro.tienda.Entity.Usuarios;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -11,7 +13,7 @@ public interface UsuariosService {
     Usuarios getByIdUsuarios(Integer id);
     Usuarios saveUsuarios(Usuarios usuarios);
     Usuarios login(String usuario, String password);
-    Usuarios registrar(String usuario, String password);
+    Usuarios registrar(String usuario, String password, String email, MultipartFile foto) throws IOException;
     Usuarios updateUsuarios(Integer id, Usuarios usuarios);
     void deleteUsuarios(Integer id);
 }
